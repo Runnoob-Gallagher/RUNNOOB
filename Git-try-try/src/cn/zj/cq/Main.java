9a7e117079a24133563fee4174a01285d5827d6f
Gallagher <18225288277@163.com>
2020-06-02 16:50:54 +0800

    try

---
+++ Git-try-try/src/cn/zj/cq/Main.java
@@ -0,0 +1,10 @@
+package cn.zj.cq;
+
+public class Main {
+	public static void main(String[] args) {
+	System.out.println("First try Git");
+	System.out.println("First try Github");
+	System.out.println("HelloWorld");
+	System.out.println("Hello");
+}
+}
