e472c5aaf3c4a57d6dbeb2df5ed4888db6fc29cf
SevenhappyYJ <35823613+SevenhappyYJ@users.noreply.github.com>
2020-06-02 18:18:23 +0800

    Update Main.java

--- Git-try-try/src/cn/zj/cq/Main.java
+++ Git-try-try/src/cn/zj/cq/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	System.out.println("First try Git");
 	System.out.println("First try Github");
 	System.out.println("HelloWorld");
-	System.out.println("Hello       World");
+	System.out.println("Hello World");
+	System.out.println("母子平安");
 }
 }
