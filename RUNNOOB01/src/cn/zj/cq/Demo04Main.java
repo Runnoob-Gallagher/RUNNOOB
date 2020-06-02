3bd79259b56c61935801e1227eee60055acc0474
Gallagher <18225288277@163.com>
2020-06-03 00:08:51 +0800

    fd

--- RUNNOOB01/src/cn/zj/cq/Demo04Main.java
+++ RUNNOOB01/src/cn/zj/cq/Demo04Main.java
@@ -3,12 +3,12 @@ package cn.zj.cq;
 public class Demo04Main {
 	public static void main(String[] args) {
 		Demo04FanxinMethod df = new Demo04FanxinMethod();
-		//你传递什么类型他就是什么类型
-		df.method("这是String类型");
+		//浣犱紶閫掍粈涔堢被鍨嬩粬灏辨槸浠�涔堢被鍨�
+		df.method("杩欐槸String绫诲瀷");
 		df.method('A');
 		df.method(5);
 		df.method(true);
-		System.out.print("666");
-		Demo04FanxinMethod.jump("传递String");
+		System.out.print("999");
+		Demo04FanxinMethod.jump("浼犻�扴tring");
 	}
 }
