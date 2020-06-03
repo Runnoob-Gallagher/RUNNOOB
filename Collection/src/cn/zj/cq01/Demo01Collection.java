b0b063a0cfa15fe3a8a57cfada54aa517e718dbb
Gallagher <18225288277@163.com>
2020-06-03 12:25:03 +0800

    Collection

---
+++ Collection/src/cn/zj/cq01/Demo01Collection.java
@@ -0,0 +1,11 @@
+package cn.zj.cq01;
+
+import java.util.Collection;
+
+//这个demo用以验证直接使用Collection创建的对象和多态之间的区别
+public class Demo01Collection {
+	public static void main(String[] args) {
+		//Collection collA = new Collection();这里接口不能创建对象的
+		
+	}
+}
