c81747b3b51a1ed3ddd9eaaffb65cd7d447f1212
Gallagher <18225288277@163.com>
2020-06-03 09:54:57 +0800

    开始工作

--- Git-try-try/src/cn/zj/cq/Main.java
+++ Git-try-try/src/cn/zj/cq/Main.java
@@ -2,10 +2,6 @@ package cn.zj.cq;
 
 public class Main {
 	public static void main(String[] args) {
-	System.out.println("First try Git");
-	System.out.println("First try Github");
-	System.out.println("HelloWorld");
-	System.out.println("Hello World");
-	System.out.println("UZI");
-}
+		System.out.println("First try Github");
+	}
 }
