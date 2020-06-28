01baf1edf6df240909485c43d4807c7f072b94c0
Gallagher <18225288277@163.com>
2020-06-28 19:32:01 +0800

    ddddsds

---
+++ ThreadTest/src/cn/zj/cq/Demo02Totoerise.java
@@ -0,0 +1,36 @@
+package cn.zj.cq;
+
+public class Demo02Totoerise implements Runnable{
+	private static String winner ;
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		for(int i=0;i<=20;i++){
+			boolean flag = Over(i);
+				if(flag ) {
+					break;
+				} 
+			
+			System.out.println("当前是" + Thread.currentThread().getName()+"跑了"+i+"米");
+		}
+		
+	}
+	public boolean Over(int steps) {
+		if(winner != null) {//说明已经存在胜利者了
+			return true;
+		}else { 
+			if(steps >= 20) {
+				winner = Thread.currentThread().getName();
+				System.out.println("winner就是" + winner);
+				 return true; 
+			}{
+				return false;
+			}
+		}
+	}
+	public static void main(String[] args) {
+		Demo02Totoerise dt = new Demo02Totoerise();
+		new Thread(dt, "兔子").start();
+		new Thread(dt, "乌龟").start();
+	}
+}
