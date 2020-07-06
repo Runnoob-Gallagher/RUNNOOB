70a17b0f31b5a3dbf87c7ea450daf444f277443d
Gallagher <18225288277@163.com>
2020-07-07 00:20:52 +0800

    二二

--- Git-try-try/src/匿名内部类/Demo02Test.java
+++ Git-try-try/src/匿名内部类/Demo02Test.java
@@ -3,18 +3,19 @@ package 匿名内部类;
 class Demo02Test{
 	 void A() {
 		class B{ 
-			B b = new B();
-			b.C();
-			void C() { 
+			void C(){ 
 				class D{
-					D d = new D();
-					d.E();
-					void E() {
+					//    d.E();开始我是写在这里的，但是呢报错：因为方法E()还没有定义呢，你怎么调用
+						void E() {
 						System.out.println("怎么访问呢：因为局部内部类在方法中，只能通过方法来访问，就从内至外一层一层访问");
 					}
 				}
+				D d = new D();
+				d.E();
 			}
 		}
+		B b = new B();
+		b.C();
 	}
 	 public static void main(String[] args) {
 		Demo02Test dt = new Demo02Test();
