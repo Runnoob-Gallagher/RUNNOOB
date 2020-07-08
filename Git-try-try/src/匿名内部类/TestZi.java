2bfa1582e9c3cee253566feafa089412a8e939f7
Gallagher <18225288277@163.com>
2020-07-09 00:52:35 +0800

    是多少

---
+++ Git-try-try/src/匿名内部类/TestZi.java
@@ -0,0 +1,15 @@
+package 匿名内部类;
+
+public class TestZi extends Test implements TestImp{
+@Override
+public void method() {
+	// TODO Auto-generated method stub
+	System.out.println("我是zi类方法");
+}
+
+@Override
+public void run() {
+	// TODO Auto-generated method stub
+	System.out.println("我是实现类方法");
+}
+}
