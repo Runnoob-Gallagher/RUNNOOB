f4e3133642d8eefdd7cc5858ae6db1d3175e6040
Gallagher <18225288277@163.com>
2020-08-04 18:37:34 +0800

    实打实大所

---
+++ 第一轮复习/src/数据类型转换/Demo02.java
@@ -0,0 +1,8 @@
+package 数据类型转换;
+
+public class Demo02 {
+	public static void main(String[] args) {
+		int a = (3 == 4? 1 : 2);
+		System.out.println(a);
+	}
+}
---
+++ 第一轮复习/src/数据类型转换/Demo04.java
@@ -0,0 +1,26 @@
+package 数据类型转换;
+
+import java.util.Scanner;
+
+public class Demo04 {
+	public static void main(String[] args) {
+		String today = "星期一";
+		switch(today) {
+		case "星期一":
+			System.out.println("开始上班");
+//			break;
+		case "星期二":
+			System.out.println("厌倦上班");
+//			break;
+		case "星期三":
+			System.out.println("睡觉");
+//			break;
+		case "星期四":
+			System.out.println("开始混日子");
+//			break;
+		case "星期五":
+			System.out.println("开始放假");
+//			break;
+		}
+	}
+}
