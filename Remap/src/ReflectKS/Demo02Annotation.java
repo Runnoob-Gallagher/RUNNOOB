478a922803f6046cdf6aab28cc86ba01ab873b01
Gallagher <18225288277@163.com>
2020-08-12 00:26:37 +0800

    发生大事

--- Remap/src/ReflectKS/Demo02Annotation.java
+++ Remap/src/ReflectKS/Demo02Annotation.java
@@ -1,5 +1,5 @@
 package ReflectKS;
-
+//重点 ：把字节视为表的数据
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -7,11 +7,11 @@ import java.lang.annotation.Target;
 
 public class Demo02Annotation {
 	//注解可以显示赋值
-	@MyAnnotationA(name = "Gallagher",school = "TSUPT")
+	@MyAnnotationAC(name = "Gallagher",school = "TSUPT")
 	public void methodA() {
 		System.out.println("测试注解的");
 	}
-	@MyAnnotationB("Gallagher") //如果只有一个值，就可以使用value作为变量，且可以省略
+	@MyAnnotationBC("Gallagher") //如果只有一个值，就可以使用value作为变量，且可以省略
 	public void methodB() {
 		System.out.println("测试注解只有一个变量的");
 	}
@@ -19,7 +19,7 @@ public class Demo02Annotation {
 	
 @Retention(value = RetentionPolicy.RUNTIME)	
 @Target(value = ElementType.METHOD)
-@interface MyAnnotationA{
+@interface MyAnnotationAC{
 	//定义注解的参数 格式： 参数类型   参数名();
 	String name() default "";  //设置默认值。  默认是这样操作的？？？？  注意一下
 	int age() default -1;//-1代表可能不存在
@@ -27,6 +27,6 @@ public class Demo02Annotation {
 }
 @Retention(value = RetentionPolicy.RUNTIME)
 @Target(value = {ElementType.METHOD,ElementType.FIELD})
-@interface MyAnnotationB{
+@interface MyAnnotationBC{
 	String value();//如果只有一个值，就可以使用value作为变量，且可以省略
 }
--- Remap/src/ReflectKS/Demo10ReflectAnnnotation.java
+++ Remap/src/ReflectKS/Demo10ReflectAnnnotation.java
@@ -1,12 +1,36 @@
 package ReflectKS;
 
-public class Demo10ReflectAnnnotation {
-	
-}
+import java.lang.annotation.Annotation;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Field;
 
-class Person{
-	private String name = "Gallagher";
-	private int age = 24;
+public class Demo10ReflectAnnnotation {
+	public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException, SecurityException {
+		Class claA = Class.forName("ReflectKS.SuperMan");  
+		Annotation[] annoA = claA.getAnnotations();
+		for (Annotation annotation : annoA) {
+			System.out.println(annotation);//@ReflectKS.MyAnnotationA(value=Table)
+		}
+		//获取注解具体的值
+		MyAnnotationA annoB = (MyAnnotationA)claA.getAnnotation(MyAnnotationA.class); //必须要强转才能有value这个方法欸
+		System.out.println(annoB.value());
+		MyAnnotaionB annoC = (MyAnnotaionB)claA.getAnnotation(MyAnnotaionB.class);
+		//获得指定的注解，比如你按annoC的方式想获得指定的注解，是不行的，因为要指定到对应的属性上去。比如db_XM
+		Field field = claA.getDeclaredField("name");
+		MyAnnotaionB annoD = (MyAnnotaionB)field.getAnnotation(MyAnnotaionB.class);
+		System.out.println(annoD);
+		System.out.println(annoD.db_NL());//这里你就可以获取想要的注解的值
+	}
+}
+@MyAnnotationA("Table") //我上面是对SuperMan类的映射，所以对类的注解肯定要放在对应的类上
+class SuperMan{
+	@MyAnnotaionB(db_XM="Varchar",db_NL=24,type = "Varchar2")
+	private String name;
+	@MyAnnotaionB(db_XM="Fional",db_NL=22,type = "int")
+	private int age ;
 	public String getName() {
 		return name;
 	}
@@ -19,13 +43,26 @@ class Person{
 	public void setAge(int age) {
 		this.age = age;
 	}
-	public Person(String name, int age) {
+	public SuperMan(String name, int age) {
 		this.name = name;
 		this.age = age;
 	}
-	public Person() {
+	public SuperMan() {
 	}
 	public void methodA() {
 		System.out.println("我就是一个普通方法");
 	}
+}
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD,ElementType.METHOD})
+@interface MyAnnotaionB{
+	String db_XM();
+	String type();
+	int db_NL();
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE,ElementType.METHOD})
+@interface MyAnnotationA{
+	String value();
 }
